d30cfb399e7dd0b976a7488b5271652472a0df05
darrickpang <dkypang@yahoo.com>
2020-11-16 16:37:44 -0800

    added more stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_secure_password :recovery_password, validations: false
+
+    validates :name, uniqueness: true
+    validates :name, :age, presence: true
+    validate :adult_age 
+
+    def adult_age
+        if age < 18
+            errors.add(:age, 'You need to be 18 before you can sign up.')
+        end
+    end
 end
